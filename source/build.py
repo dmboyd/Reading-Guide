78aaf5a84ad057f339a574b07a91cdcc010f6a69
David Boyd <dmboyd@gmail.com>
2026-01-31 16:39:42 -0500

    update css

--- source/build.py
+++ source/build.py
@@ -11,5 +11,6 @@ build_webfonts(DEFAULT_INPUT, DEFAULT_OUTPUT)
 DOCS.mkdir(exist_ok=True)
 shutil.copy2(DEFAULT_OUTPUT / "ReadingGuide-Regular.woff", DOCS)
 shutil.copy2(DEFAULT_OUTPUT / "ReadingGuide-Regular.woff2", DOCS)
+shutil.copy2(DEFAULT_OUTPUT / "reading-guide.css", DOCS)
 
 print("Copied to docs/")
